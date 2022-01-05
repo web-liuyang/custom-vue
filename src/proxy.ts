7542e9fa74bd34a914e3adce5ff3e29e6f6cd6da
web-liuyang <984584014@qq.com>
2022-01-05 14:01:47 +0800

    fix: delete redundant code

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,6 @@
 import { Context } from "./context";
 import { scheduleQueue } from "./scheduler";
 
-interface MapValue {
-  updater: Function[];
-}
-
-export let map: Map<string | symbol, MapValue> = new Map();
-
 /** 创建响应式对象 */
 export const reactive = <T extends object>(obj: T, ctx?: Context): T => {
   // 在这个对象的时候我就要知道 需要触发更新的函数是什么
@@ -14,9 +8,6 @@ export const reactive = <T extends object>(obj: T, ctx?: Context): T => {
     get(target: T, p: string | symbol, receiver: any) {
       const value = Reflect.get(target, p, receiver);
 
-      // if (!map.has(p)) {
-      //   map.set(p, { updater: [] });
-      // }
       return value;
     },
     set(target: T, p: string, value: any, receiver: any) {
